139359948d9e9dfea8866ef4b6074ccc6f3fe421
Wheezyyy <th4vort4x@gmail.com>
2019-01-22 19:10:03 +0100

    Video tabelle

---
+++ Video.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Videos
+(
+VideoID INTEGER,
+Titel VARCHAR (30),
+Kategorie VARCHAR (30),
+Aufrufe NUMERIC,
+Likes NUMERIC,
+Dislikes NUMERIC,
+Kommentare NUMERIC,
+UPLOADATUM DATE,
+);
+
+   insert into Nutzer
+   values (111,Lets_Play_ARK,Gaming,1050585,2946,352,1389,4-5-2018);
+   insert into Nutzer
+   values (222,FIFA_Hymne,Musik,487974,45032,2001,2944,1-1-2019);
+   insert into Nutzer
+   values (333,Bitch_Lasagna,Musik,107288672,5400000,584438,826174,5-10-2018);
+   insert into Nutzer
+   values (444,Das_ERSTE_HAUSTIER_von_ApoRed,Unterhaltung,840814,66877,1598,12134,02-04-2018);
+   insert into Nutzer
+   values (555,AUSRASTER_von_STREAMERN,Unterhaltung,289131,19624,279,1498,19-01-2019);
+   insert into Nutzer
+   values (666,Man_wendet_sich_nicht_gegen_Spandau,Unterhaltung,1723361,116417,614,6826,05-07-2017);
+   insert into Nutzer
+   values (777,NEUER_KRASSER_PC,Gaming,260960,14004,289,2063,14-08-2018);
+   insert into Nutzer
+   values (888,Hello_Neighbour,Gaming,278747,11084,85,391,24-12-2018);
+   insert into Nutzer
+   values (999,Das_einzig_WAHRE_Spiderman_Videospiel,Gaming,275882,28074,212,4189,06-11-2018);
+   insert into Nutzer
+   values (101010,Transgender_im_Kampfsport,Unterhaltung,168393,14818,852,3001,8-1-2019);
+
+
